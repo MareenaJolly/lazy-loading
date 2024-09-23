177c81180d858def66944152baf9a4c2e7b77edc
mareena <mereena333@gmail.com>
2024-09-23 12:01:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { Suspense, lazy } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './Components/Home';
+
+const About = lazy(() => import('./Components/About'));
+const Contact = lazy(() => import('./Components/Contact'));
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
